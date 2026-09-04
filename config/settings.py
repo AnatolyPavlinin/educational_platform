be3c608179aba3065ca71197b3713137c4138967
Anatoly Pavlinin <anatolianych@gmail.com>
2026-09-04 11:09:38 +0300

    fix: check CI before accessing DB_NAME

--- config/settings.py
+++ config/settings.py
@@ -91,16 +91,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': 'db',
-        'PORT': '5432',
+if os.getenv("CI") == "true":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "ci.sqlite3",
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
+            'HOST': 'db',
+            'PORT': '5432',
+        }
+    }
 
 if os.getenv("CI") == "true":
     DATABASES = {
